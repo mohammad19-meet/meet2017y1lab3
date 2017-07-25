958db9c7dc8d2c1079598b0c66d7845d22155c4f
mohammad19-meet <mohammad19@meet.mit.edu>
2017-07-25 15:47:38 +0200

    Stopping here

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+print('hello world')
+hello world
+>>> print('नमस्ते दुनिया')
+नमस्ते दुनिया
+>>> 
---
+++ specialCharacters.py
@@ -0,0 +1,9 @@
+print(" \”Hi!\” ”)
+      
+print(" \’Hello!\’ ”)
+      
+print(" \\MEET\\Module1\\“)
+      
+print("first \n second”)
+      
+print("no tab \t tab”)
